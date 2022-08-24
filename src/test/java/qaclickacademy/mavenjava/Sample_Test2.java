749e027822eac7000850d19e4443de72dd0106af
satyabat <satya.batchu@gmail.com>
2022-08-24 19:50:52 +0530

    first file

---
+++ src/test/java/qaclickacademy/mavenjava/Sample_Test2.java
@@ -0,0 +1,24 @@
+package qaclickacademy.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class Sample_Test2 {
+
+	@Test
+	public void sample()
+	{
+		System.out.println("First project in Maven");
+	}
+	
+@Test 
+public void second_sample()
+
+{
+	System.out.println("second program");
+}
+@Test
+public void third_sample()
+{
+	System.out.println("Maven Program");
+}
+}
